ddeac6bf515a8cd3adbfb825977d594bd0e07bfd
魏佳探 <442774948@qq.com>
2018-04-10 16:42:22 +0800

    工时管理 - 路由更改，同时新引入一个日期组件

---
+++ src/page/designer/hours.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="page_hours">
+        <el-tabs v-model="activeName" @tab-click="handleClick">
+            <el-tab-pane label="我的日程" name="first">我的日程</el-tab-pane>
+            <el-tab-pane label="所有日程" name="second">所有日程</el-tab-pane>
+        </el-tabs>
+    </div>
+</template>
+<script>
+  export default {
+    data() {
+        return {
+            activeName: 'first'
+        };
+    },
+    methods: {
+        handleClick(tab, event) {
+            console.log(tab, event);
+        }
+    }
+  };
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ const login = r => require.ensure([], () => r(require('../page/login/login.vue')
 const home = r => require.ensure([], () => r(require('../page/home/home.vue')), '主页');
 const custom = r => require.ensure([], () => r(require('../page/custom/custom.vue')), '客户列表页');
 const designer = r => require.ensure([], () => r(require('../page/designer/designer')), '设计师列表');
-const designerHours = r => require.ensure([], () => r(require('../page/designer/children/hours')), '工时上报');
+const hours = r => require.ensure([], () => r(require('../page/designer/hours')), '工时上报');
 
 export default [
     {
@@ -16,16 +16,14 @@ export default [
             {
                 path: '/custom/:id',
                 component: custom
-            }
-        ]
-    },
-    {
-        path: '/designer',
-        component: designer,
-        children:[
+            },
             {
-                path: '/designerHours',
-                component: designerHours
+                path: '/designer',
+                component: designer
+            },
+            {
+                path: '/hours',
+                component: hours
             }
         ]
     }
